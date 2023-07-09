1e1ec9460e7bbb7c0da551fe040bf3695ae49be9
justchokingaround <ivanonarch@tutanota.com>
2023-07-09 14:04:24 +0200

    feat: add listview

--- app/src/main/java/smart/cart/client/MainActivity.java
+++ app/src/main/java/smart/cart/client/MainActivity.java
@@ -1,13 +1,22 @@
 package smart.cart.client;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.textfield.TextInputEditText;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    private List<String> sampleItems = new ArrayList<>(Arrays.asList("Tomatoes", "Potatoes", "Cucumber", "Milk", "Cheese"));
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,9 +26,16 @@ public class MainActivity extends AppCompatActivity {
         final Button button = findViewById(R.id.mainButton);
         button.setOnClickListener(view -> {
             if (textInputEditText.getText() != null && textInputEditText.getText().length() != 0) {
-                // Switch to the items list layout
+                String cartId = textInputEditText.getText().toString();
                 setContentView(R.layout.items_list);
+                setTitle("Cart Items - " + cartId);
+                ArrayAdapter adapter = new ArrayAdapter<String>(this,
+                        android.R.layout.simple_list_item_1,
+                        sampleItems);
+                ListView listView = (ListView) findViewById(R.id.itemsList);
+                listView.setAdapter(adapter);
             }
         });
     }
+
 }
