6f43c577403ce707a9cf2c48ca92e8aa5cf0b132
justchokingaround <ivanonarch@tutanota.com>
2023-07-15 05:46:27 +0200

    feat: add dynamic items fetching

--- app/src/main/java/smart/cart/client/MainActivity.java
+++ app/src/main/java/smart/cart/client/MainActivity.java
@@ -1,7 +1,6 @@
 package smart.cart.client;
 
 import static smart.cart.client.shared.Helpers.associateTitleToImage;
-import static smart.cart.client.shared.Helpers.refreshList;
 
 import android.os.Bundle;
 import android.widget.Button;
@@ -9,97 +8,106 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import com.google.android.material.textfield.TextInputEditText;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
+import smart.cart.client.shared.Helpers;
 import smart.cart.client.shared.Item;
 import smart.cart.client.shared.MyListAdapter;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ListView list;
     private double totalPrice = 0;
-    Item[] items = {
-            new Item("000001", "Tomatoes", 1.99, 1),
-            new Item("000002", "Potatoes", 2.99, 1),
-            new Item("000003", "Cucumber", 0.99, 6),
-            new Item("000004", "Milk", 3.99, 1),
-            new Item("000005", "Cheese", 4.99, 1),
-            new Item("000006", "Bread", 1.99, 1),
-            new Item("000007", "Eggs", 2.99, 1),
-            new Item("000008", "Butter", 0.99, 1),
-            new Item("000009", "Ham", 3.99, 1),
-            new Item("000010", "Sausage", 4.99, 1),
-            new Item("000011", "Chicken", 1.99, 1),
-            new Item("000012", "Beef", 2.99, 1),
-            new Item("000013", "Fish", 3.99, 1),
-            new Item("000014", "Rice", 4.99, 1),
-    };
-    List<Item> itemsList = new ArrayList<>(Arrays.asList(items));
-    String[] mainTitle = new String[itemsList.size()];
-    String[] subtitle = new String[itemsList.size()];
-    Integer[] imgId = new Integer[itemsList.size()];
+    private ExecutorService executorService;
 
+    private TextInputEditText textInputEditText;
+    private TextView totalPriceTextView;
+    private ListView itemsListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final TextInputEditText textInputEditText = findViewById(R.id.cartIdInput);
-        final Button button = findViewById(R.id.mainButton);
+        textInputEditText = findViewById(R.id.cartIdInput);
+        Button button = findViewById(R.id.mainButton);
         button.setOnClickListener(view -> {
-            if (textInputEditText.getText() != null && textInputEditText.getText().length() != 0) {
-                String cartId = textInputEditText.getText().toString();
-                setContentView(R.layout.items_list);
-                setTitle("Cart ID: " + cartId);
-
-                for (int i = 0; i < itemsList.size(); i++) {
-                    mainTitle[i] = itemsList.get(i).getName();
-                    subtitle[i] = itemsList.get(i).getQuantity() + " x " + itemsList.get(i).getPrice() + "€";
-                    imgId[i] = associateTitleToImage(itemsList.get(i).getName());
-                    if (itemsList.get(i).getPrice() instanceof Double d) {
-                        if (itemsList.get(i).getQuantity() instanceof Integer q) {
-                            totalPrice += d * q;
-                        } else {
-                            totalPrice += d;
-                        }
-                    } else {
-                        if (itemsList.get(i).getQuantity() instanceof Integer q) {
-                            totalPrice += (itemsList.get(i).getPrice()).doubleValue() * q;
-                        } else {
-                            totalPrice += (itemsList.get(i).getPrice()).doubleValue();
-                        }
-                    }
-                }
-                totalPrice = Math.round(totalPrice * 100.0) / 100.0;
-                TextView textView = findViewById(R.id.totalPrice);
-                String totalPriceString = "Total: " + totalPrice + "€";
-                textView.setText(totalPriceString);
-                textView.setTextAlignment(TextView.TEXT_ALIGNMENT_VIEW_END);
-                MyListAdapter adapter = new MyListAdapter(this, mainTitle, subtitle, imgId);
-                list = findViewById(R.id.itemsList);
-                list.setAdapter(adapter);
-
-                // add a refresh button
-                final Button refreshButton = findViewById(R.id.refresh);
-
-                refreshButton.setOnClickListener(v -> {
-                    refreshList(this);
-                });
-
-//                SwipeRefreshLayout swipeRefreshLayout = findViewById(R.id.swipeRefresh);
-//                swipeRefreshLayout.setOnRefreshListener(() -> {
-//                    refreshList(this);
-//                });
-
+            String cartId = Objects.requireNonNull(textInputEditText.getText()).toString().trim();
+            if (!cartId.isEmpty()) {
+                initializeItemListLayout(cartId);
+                fetchItemsAndUpdateUI(cartId);
             }
         });
+
+        executorService = Executors.newSingleThreadExecutor();
     }
 
+    private void initializeItemListLayout(String cartId) {
+        setContentView(R.layout.items_list);
+        setTitle("Cart ID: " + cartId);
+
+        Button refreshButton = findViewById(R.id.refresh);
+        refreshButton.setOnClickListener(v -> refreshItems(cartId));
+
+        totalPriceTextView = findViewById(R.id.totalPrice);
+        itemsListView = findViewById(R.id.itemsList);
+    }
+
+    private void fetchItemsAndUpdateUI(String cartId) {
+        executorService.submit(() -> Helpers.getItems(cartId, receivedItems -> runOnUiThread(() -> {
+            processItems(receivedItems);
+            updateTotalPriceUI();
+        })));
+    }
+
+    private void processItems(Item[] receivedItems) {
+        List<Item> itemsList = new ArrayList<>(Arrays.asList(receivedItems));
+        String[] mainTitle = new String[itemsList.size()];
+        String[] subtitle = new String[itemsList.size()];
+        Integer[] imgId = new Integer[itemsList.size()];
+
+        for (int i = 0; i < itemsList.size(); i++) {
+            Item item = itemsList.get(i);
+            mainTitle[i] = item.getName();
+            subtitle[i] = item.getQuantity() + " x " + item.getPrice() + "€";
+            imgId[i] = associateTitleToImage(item.getName());
+            updateTotalPrice(item);
+        }
+
+        MyListAdapter adapter = new MyListAdapter(this, mainTitle, subtitle, imgId);
+        itemsListView.setAdapter(adapter);
+    }
+
+    private void updateTotalPrice(Item item) {
+        double price = item.getPrice().doubleValue();
+        int quantity = (int) item.getQuantity();
+
+        totalPrice += price * quantity;
+    }
+
+    private void updateTotalPriceUI() {
+        totalPrice = Math.round(totalPrice * 100.0) / 100.0;
+        String totalPriceString = "Total: " + totalPrice + "€";
+        totalPriceTextView.setText(totalPriceString);
+        totalPriceTextView.setX(totalPriceTextView.getX() + totalPriceTextView.getWidth() - totalPriceTextView.getPaint().measureText(totalPriceString) - 20);
+    }
+
+    private void refreshItems(String cartId) {
+        initializeItemListLayout(cartId);
+        totalPrice = 0;
+        fetchItemsAndUpdateUI(cartId);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        executorService.shutdown();
+    }
 }
--- app/src/main/java/smart/cart/client/shared/Helpers.java
+++ app/src/main/java/smart/cart/client/shared/Helpers.java
@@ -1,15 +1,55 @@
 package smart.cart.client.shared;
 
-import android.app.Activity;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 import smart.cart.client.R;
 
 public class Helpers {
+    public interface GetItemsCallback {
+        void onItemsReceived(Item[] items);
+    }
 
+    public static void getItems(String cartId, GetItemsCallback callback) {
+        String url = "https://smart-cart-service.onrender.com/items";
 
-    public static void refreshList(Activity context) {
-        context.setContentView(R.layout.items_list);
-        context.setTitle("TODO");
+        try {
+            String requestUrl = url + "?cartId=" + cartId;
+            URL requestURL = new URL(requestUrl);
+            HttpURLConnection connection = (HttpURLConnection) requestURL.openConnection();
+            connection.setRequestMethod("GET");
+            int responseCode = connection.getResponseCode();
+            if (responseCode == HttpURLConnection.HTTP_OK) {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                StringBuilder responseBody = new StringBuilder();
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    responseBody.append(line);
+                }
+                reader.close();
+
+                JSONArray jsonArray = new JSONArray(responseBody.toString());
+                Item[] items = new Item[jsonArray.length()];
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    String itemId = jsonArray.getJSONObject(i).getString("itemId");
+                    String name = jsonArray.getJSONObject(i).getString("name");
+                    Double price = jsonArray.getJSONObject(i).getDouble("price");
+                    Integer quantity = jsonArray.getJSONObject(i).getInt("quantity");
+                    items[i] = new Item(itemId, name, price, quantity);
+                }
+                callback.onItemsReceived(items);
+            }
+
+        } catch (IOException | JSONException e) {
+            e.printStackTrace();
+        }
+
     }
 
     public static Integer associateTitleToImage(String title) {
@@ -29,9 +69,10 @@ public class Helpers {
             case "Beef" -> R.drawable.beef;
             case "Fish" -> R.drawable.fish;
             case "Rice" -> R.drawable.rice;
+            case "CocaCola" -> R.drawable.cocacola;
+            case "Nutela" -> R.drawable.nutela;
             default -> R.drawable.ic_launcher_foreground;
         };
 
     }
-
 }
